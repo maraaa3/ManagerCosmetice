4a0cad7e24de2caf9617b354a1183bcc7e450880
maraaa3 <mara.broscatan@yahoo.com>
2025-11-29 17:18:46 +0200

    primul commit

---
+++ src/main/java/com/proiect/ManagerCosmetice/ManagerCosmeticeApplication.java
@@ -0,0 +1,13 @@
+package com.proiect.ManagerCosmetice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerCosmeticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerCosmeticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proiect/ManagerCosmetice/ManagerCosmeticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiect.ManagerCosmetice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerCosmeticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
